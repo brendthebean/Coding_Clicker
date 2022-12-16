d47920d08041dd1f7a1e163248a653d3ab664e16
Brenden Smith <brendthebean@gmail.com>
2022-12-16 17:26:23 -0600

    committing not quitting

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 
 let clickForPoints = document.getElementById("codeToClick");
-
 let scoreBoard = document.getElementById("totalScore");
+let ideClicker = document.getElementById("ide");
 
 let points = 0;
+let ide = 0;
 
 const givePoints = () => {
     ++points;
     scoreBoard.innerText = points;
 }
 
+const ideAutoClick = () => {
+    points -= 10;
+    ++ide;
+    
+}
 
 clickForPoints.addEventListener('click', givePoints);
+ideClicker.addEventListener('click', ideAutoClick);
